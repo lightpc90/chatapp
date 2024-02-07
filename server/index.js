746291492267866cd078cf1f93ccde4203b0f530
lightpc90 <afoageektech@gmail.com>
2024-02-07 15:29:07 +0100

    made the client side to run on the same host with server by moving the client code to public folder in the server dir

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
-import { createServer } from "http";
+import express from "express";
 import { Server } from "socket.io";
+import path from 'path'
+import { fileURLToPath } from "url";
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const PORT = process.env.PORT || 3500
+const app = express()
+app.use(express.static(path.join(__dirname, "public")))
+
+const expressServer = app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+const io = new Server(expressServer, {
   cors: {
     origin:
       process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"],
@@ -19,4 +29,3 @@ io.on("connection", (socket) => {
 });
 
 
-httpServer.listen(3200, () => { console.log(`listening on port 3200`) })
